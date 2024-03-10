1ce5e854307184f9100d481982483d74fa3983a8
Djennkins <djennkins228@gmail.com>
2024-03-10 09:54:15 +0200

    created starting project

---
+++ src/features/icecream/IcecreamView.jsx
@@ -0,0 +1,9 @@
+export const IcecreamView = () => {
+	return (
+		<div>
+			<h2>Number of ice creams - </h2>
+			<button>Order ice creams</button>
+			<button>Restock ice creams</button>
+		</div>
+	);
+};
